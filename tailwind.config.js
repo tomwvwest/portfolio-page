48932d1c77e305bfaa7712764f1aaff8a8a21d0b
tomwvwest <tomwvwest@gmail.com>
2024-01-26 15:41:16 +0000

    start of header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      blue: '##001F3F',
+      smoke: '#f2f2f2',
+      gold: '#FFD700',
+      red: '#fe5f55',
+      jet: '#333333'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
